e05d7e44b90f07d6a8d100c95821424741434f22
Kenan Kadic <ken.kadic@outlook.com>
2026-08-04 13:04:15 -0500

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -213,7 +213,7 @@ export default defineUserConfig({
               '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Using_Manage_Files_to_Download',
               '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Downloading_One_Image_From_Session',
               '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Downloading_Sessions_from_CNDA',
-              '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Downloading_Sessions_from_CNDA',
+              '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Downloading_Sessions_via_Script',
               '/CNDA_User_Guide_and_Tutorials/Downloading_Data/Bulk_Downloads_from_CNDA_to_Windows_PC',
             ],
           },
